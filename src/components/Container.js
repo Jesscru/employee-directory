040082ca86a9e2efc9cd88e94077c7096d821d05
jesscru <jessicacrump7@gmail.com>
2021-01-17 23:36:44 -0700

    added and styled buttons to search form and created onclick functions that do not currently work

--- src/components/Container.js
+++ src/components/Container.js
@@ -8,23 +8,26 @@ import Header from "./Header";
 class Container extends Component {
     state = {
       people: [],
-      search: ""
+      names: [],
+      search: "",
+      locations: ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
     };
   
-    // When this component mounts, search for the movie "The Matrix"
+  // when this component mounts, query the database for all of the people
     componentDidMount() {
       this.getPeople();
     }
   
+    // api call to the random user api and stores the results in an object
     getPeople = query => {
       API(query)
         .then(res => {
           this.setState({ people: res.data.results })
-          console.log(this.state.people);
         })
         .catch(err => console.log(err));
     };
   
+  // allows the input field to be entered and sets the value of the field to whatever is being typed in  
     handleInputChange = event => {
       const value = event.target.value;
       const name = event.target.name;
@@ -32,18 +35,35 @@ class Container extends Component {
         [name]: value
       });
     };
-  
+
+  // filters employees by the (last) name entered in the input field
+  handleFormSubmit = event => {
+    event.preventDefault();
+
+    const inputValue = document.querySelector("#filter").value;
+    const filteredName = this.state.people.filter(name => name.name.last === inputValue);
+    this.setState({
+      names: filteredName
+    });
+  }
+
+  handleSort = () => {
+    this.setState({
+      names: this.state.names.sort((a,b) => a.name.last.localeCompare(b.name.last))
+    });
+  }
   
   render() {
     return (
         <div>
           <Header />
             <SearchForm
-                value={this.state.search}
-                handleInputChange={this.handleInputChange} />
+                // value={this.state.search}
+                handleInputChange={this.handleInputChange}
+                handleSort={this.handleSort}
+                />
                 
             <ResultsTable people={this.state.people}/>
-
         </div>
       );
     }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import "../styling/SearchForm.css";
 
 
 function SearchForm(props){
@@ -7,13 +9,26 @@ function SearchForm(props){
         <div>
           <form>
             <input
+                id="filter"
                 className="form-control"
                 type="text"
-                placeholder="Search employees by state"
-                name="search"
+                placeholder="Filter by last name"
+                name="filter"
                 value={props.value}
                 onChange={props.handleInputChange}
               />
+            <Button 
+              className="filter-btn"
+              variant="outline-primary" 
+              onClick={props.handleFormSubmit}> Filter
+            </Button>
+
+            <Button 
+              className="sort-btn"
+              variant="outline-primary" 
+              onClick={props.handleSort}>Sort by last name
+            </Button>
+
           </form>
         </div>
     );
